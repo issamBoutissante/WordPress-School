d6549a89b696bb8d76aa94f191ff1b2d3669215f
issamBoutissante <boutissante.issam.dev@gmail.com>
2021-01-28 11:28:33 +0100

    next

--- page.php
+++ page.php
@@ -20,19 +20,28 @@ while(have_posts()){
     if($parent){
         ?>
         <div class="metabox metabox--position-up metabox--with-home-link">
-        <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent)?></a> <span class="metabox__main">Our History</span></p>
+        <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($parent)?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($parent)?></a> <span class="metabox__main"><?php the_title()?></span></p>
         </div>
         <?php
     }
     ?>
     
-    <!-- <div class="page-links">
+    <div class="page-links">
       <h2 class="page-links__title"><a href="#">About Us</a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+        <?php
+        if($parent){
+            $child=$parent;
+        }else{
+            $child=get_the_ID();
+        }
+        wp_list_pages(array(
+            "title_li"=>null,
+            "child_of"=>$child
+        ))
+        ?>
       </ul>
-    </div> -->
+    </div>
 
     <div class="generic-content">
      <p><?php the_content()?></p>
